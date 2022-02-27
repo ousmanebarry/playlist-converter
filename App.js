d0ad3333a096c61c6fd20736bf0cfeedd3c9a963
Ousmane Barry <ppobarry@gmail.com>
2022-02-26 20:52:41 -0500

    Logout using navigation bar

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { LogBox } from 'react-native';
+import { signOut } from 'firebase/auth';
 import { auth } from './app/config/firebase';
 import HomeScreen from './app/screens/HomeScreen';
 import { onAuthStateChanged } from 'firebase/auth';
@@ -8,10 +9,10 @@ import LoginScreen from './app/screens/LoginScreen';
 import SignupScreen from './app/screens/SignupScreen';
 import ProfileScreen from './app/screens/ProfileScreen';
 import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerItem } from '@react-navigation/drawer';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-LogBox.ignoreLogs(['Setting a timer', 'AsyncStorage has been extracted from react-native', 'Require cycle: App.js']);
+LogBox.ignoreLogs(['Setting a timer', 'AsyncStorage has been extracted from react-native']);
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -19,6 +20,15 @@ const Drawer = createDrawerNavigator();
 function App() {
   const [user, setUser] = React.useState(null);
   const [loading, setLoading] = React.useState(true);
+
+  const handleSignOut = async () => {
+    try {
+      await signOut(auth);
+      setUser(null);
+    } catch (error) {
+      alert(error.message);
+    }
+  };
 
   React.useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -42,7 +52,17 @@ function App() {
       ) : (
         <AuthContext.Provider value={[user, setUser]}>
           <NavigationContainer independent>
-            <Drawer.Navigator initialRouteName='Home'>
+            <Drawer.Navigator
+              initialRouteName='Home'
+              drawerContent={(props) => {
+                return (
+                  <DrawerContentScrollView {...props}>
+                    <DrawerItemList {...props} />
+                    <DrawerItem label='Logout' onPress={handleSignOut} />
+                  </DrawerContentScrollView>
+                );
+              }}
+            >
               <Drawer.Screen name='Home' component={HomeScreen} />
               <Drawer.Screen name='Profile' component={ProfileScreen} />
             </Drawer.Navigator>
--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,29 +1,14 @@
 import React from 'react';
-import { AuthContext } from '../../App';
-import { signOut } from 'firebase/auth';
 import { auth } from '../config/firebase';
-import { StyleSheet, Text, TouchableOpacity, View, Platform, StatusBar, Image } from 'react-native';
+import { StyleSheet, Text, View, Platform, StatusBar } from 'react-native';
 
 const HomeScreen = () => {
-  const [user, setUser] = React.useContext(AuthContext);
-
-  const handleSignOut = async () => {
-    try {
-      await signOut(auth);
-      setUser(null);
-    } catch (error) {
-      alert(error.message);
-    }
-  };
+  const user = auth.currentUser;
 
   return (
     <View style={styles.container}>
       <Text>Email: {user?.email}</Text>
       <Text>ID: {user?.uid}</Text>
-      <Image style={styles.tinyLogo} source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}></Image>
-      <TouchableOpacity onPress={handleSignOut} style={styles.button}>
-        <Text style={styles.buttonText}>Sign out</Text>
-      </TouchableOpacity>
     </View>
   );
 };
@@ -36,22 +21,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     marginTop: Platform.OS == 'android' ? StatusBar.currentHeight : 0,
-  },
-  button: {
-    backgroundColor: '#0782F9',
-    width: '60%',
-    padding: 15,
-    borderRadius: 10,
-    alignItems: 'center',
-    marginTop: 40,
-  },
-  buttonText: {
-    color: 'white',
-    fontWeight: '700',
-    fontSize: 16,
-  },
-  tinyLogo: {
-    width: 50,
-    height: 50,
   },
 });
